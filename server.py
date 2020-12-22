8b77090e3ad9434b6129723a75fad0029fd456e1
chiahohsiung <ch3472@columbia.edu>
2020-12-22 15:13:02 -0500

    flask variable rules

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Alison!'
+@app.route('/<username>')
+def hello_world(username='Alison'):
+    return render_template('script.html', name=username)
+
+
+@app.route('/height')
+def my_height():
+    return render_template('height.html')
