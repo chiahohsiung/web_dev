cbb60eac10ed2cd732e306ba9603d74d364288c3
chiahohsiung <ch3472@columbia.edu>
2020-12-24 13:50:59 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
 @app.route('/<username>')
-def hello_world(username='Alison'):
+def user(username='Alison'):
     return render_template('script.html', name=username)
+
+@app.route('/<username>/<int:post_id>')
+def post(username='Alison', post_id=None):
+    return render_template('post.html', name=username, id=post_id)
 
 
 @app.route('/height')
